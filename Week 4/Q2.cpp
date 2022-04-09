66d81957acbec4c7137f7fedec96a627b568898a
Shantanu <papun260801@gmail.com>
2022-04-09 19:43:04 +0530

    Added W4Q2, W4Q3

--- Week 4/Q2.cpp
+++ Week 4/Q2.cpp
@@ -1,9 +1,4 @@
-// Given an unsorted array of integers, design an algorithm and implement it using a program to
-// sort an array of elements by partitioning the array into two subarrays based on a pivot element
-// such that one of the sub array holds values smaller than the pivot element while another sub
-// array holds values greater than the pivot element. Pivot element should be selected randomly
-// from the array. Your program should also find number of comparisons and swaps required for
-// sorting the array.
+// Given an unsorted array of integers, design an algorithm and implement it using a program to sort an array of elements by partitioning the array into two subarrays based on a pivot element such that one of the sub array holds values smaller than the pivot element while another sub array holds values greater than the pivot element. Pivot element should be selected randomly from the array. Your program should also find number of comparisons and swaps required for sorting the array.
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -15,30 +10,39 @@ void swap(int &a, int &b)
     b = temp;
 }
 
-void partition(vector<int> &arr, int low, int high)
+int partition(vector<int> &arr, int low, int high)
 {
-    int pivot = arr[low], i = low, j = high;
+    int pivot = low + (rand() % (high - low));
+    int i = low, j = high;
 
-    while (i <= j)
+    while (i < j)
     {
         do
+        {
             i++;
-        while (arr[i] >= pivot);
+        } while (arr[i] <= arr[pivot]);
         do
+        {
             j--;
-        while (arr[j] <= pivot);
+        } while (arr[j] > arr[pivot]);
 
         if (i < j)
             swap(arr[i], arr[j]);
     }
 
-    swap(arr[low], a[j]);
+    swap(arr[pivot], arr[j]);
 
     return j;
 }
 
-void quickSort(vector<int> &arr)
+void quickSort(vector<int> &arr, int low, int high)
 {
+    if (low < high)
+    {
+        int j = partition(arr, low, high);
+        quickSort(arr, low, j);
+        quickSort(arr, j + 1, high);
+    }
 }
 
 int main()
@@ -50,7 +54,7 @@ int main()
     for (int i = 0; i < n; i++)
         cin >> arr[i];
 
-    quickSort(arr);
+    quickSort(arr, 0, n);
 
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
---
+++ Week 4/Q3.cpp
@@ -0,0 +1,74 @@
+// Given an unsorted array of integers, design an algorithm and implement it using a program to find Kth smallest or largest element in the array.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int partition(vector<int> &arr, int low, int high)
+{
+    int pivot = arr[low], i = low, j = high;
+
+    while (i < j)
+    {
+        do
+        {
+            i++;
+        } while (arr[i] <= pivot);
+        do
+        {
+            j--;
+        } while (arr[j] > pivot);
+
+        if (i < j)
+            swap(arr[i], arr[j]);
+    }
+
+    swap(arr[low], arr[j]);
+
+    return j;
+}
+
+int quickSelect(vector<int> &arr, int low, int high, int k)
+{
+    if (low < high)
+    {
+        int j = partition(arr, low, high);
+        if (j + 1 == k)
+            return arr[j];
+        else if (j + 1 > k)
+            return quickSelect(arr, low, j, k);
+        else
+            return quickSelect(arr, j + 1, high, k);
+    }
+    else
+        return -1;
+}
+
+int main()
+{
+    int t, n, k;
+    cin >> t;
+
+    do
+    {
+        cin >> n;
+        vector<int> v(n);
+
+        for (int i = 0; i < n; i++)
+            cin >> v[i];
+
+        cin >> k;
+
+        cout << quickSelect(v, 0, n - 1, k) << "\n";
+
+        t--;
+    } while (t != 0);
+
+    return 0;
+}
